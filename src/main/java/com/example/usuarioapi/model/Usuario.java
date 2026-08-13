6164a4f7e4ec7b21dcc27c5d5fe297c93a4a3e15
Felipe Diniz <felipedinizprogamer@gmail.com>
2026-08-13 17:28:31 -0300

    feat: adiciona o model do usuario

---
+++ src/main/java/com/example/usuarioapi/model/Usuario.java
@@ -0,0 +1,36 @@
+package com.example.usuarioapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String cpf;
+    private String senha;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
