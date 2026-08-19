6edd4707a0b55aa54bd074e4b6de587d843278f9
Felipe Diniz <felipedinizprogamer@gmail.com>
2026-08-19 14:14:59 -0300

    feat: adiciona o controller usuario

---
+++ src/main/java/com/example/usuarioapi/Controller/UsuarioController.java
@@ -0,0 +1,34 @@
+package com.example.usuarioapi.Controller;
+
+import com.example.usuarioapi.model.Usuario;
+import com.example.usuarioapi.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable int id){
+        return ResponseEntity.ok(usuarioService.findById(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Usuario>> findAll(){
+        return ResponseEntity.ok(usuarioService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> save(@RequestBody Usuario usuario){
+        return ResponseEntity.ok(usuarioService.save(usuario));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        usuarioService.delete(id);
+    }
+}
