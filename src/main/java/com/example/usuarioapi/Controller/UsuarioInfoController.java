e6b6bc8e9363ea3351f0348d270c79aac30ab153
Felipe Diniz <felipedinizprogamer@gmail.com>
2026-08-19 14:18:44 -0300

    feat: adiciona o controller usuario info

---
+++ src/main/java/com/example/usuarioapi/Controller/UsuarioInfoController.java
@@ -0,0 +1,34 @@
+package com.example.usuarioapi.Controller;
+
+import com.example.usuarioapi.model.UsuarioInfo;
+import com.example.usuarioapi.service.UsuarioInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class UsuarioInfoController {
+    @Autowired
+    private UsuarioInfoService usuarioInfoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioInfo> findById(@PathVariable int id){
+        return ResponseEntity.ok(usuarioInfoService.findById(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UsuarioInfo>> findAll(){
+        return ResponseEntity.ok(usuarioInfoService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioInfo> save(@RequestBody UsuarioInfo usuarioInfo){
+        return ResponseEntity.ok(usuarioInfoService.save(usuarioInfo));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        usuarioInfoService.delete(id);
+    }
+}
